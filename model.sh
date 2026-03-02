8ff83377fc9400d39046723ce3289ce37f9e0ec0
pyxyzc <pyxyzc@gmail.com>
2026-03-02 08:12:35 +0000

    scripts

---
+++ model.sh
@@ -0,0 +1,2 @@
+curl -X GET http://127.0.0.1:30000/v1/models
+
---
+++ run-only-dram.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /sgl-workspace/sglang_data/
+rm -rf *
+
+cd /sgl-workspace/sglang/
+# 模型路径（改成你本地的，比如 /data/models/Llama-2-7b-hf）
+MODEL_PATH=/home/models/QwQ-32B
+
+# 端口号（可以改，比如 30000）
+PORT=30000
+# 卡数量
+TP=2
+
+# 只使用
+export CUDA_VISIBLE_DEVICES=2,3
+
+# 启动命令
+python python/sglang/launch_server.py \
+    --model-path $MODEL_PATH \
+    --page-size 128 \
+    --tp $TP \
+    --port $PORT \
+    --enable-hierarchical-cache
